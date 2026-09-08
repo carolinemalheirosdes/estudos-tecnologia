60f52851d6120fe7cfd8151867102ca7011c24da
Caroline Malheiros <contato.carolmalheiros@gmail.com>
2026-09-08 15:09:46 -0300

    commit

--- JavaScript/Aula Eventos/script.js
+++ JavaScript/Aula Eventos/script.js
@@ -3,3 +3,10 @@ const botao = document.querySelector("#botao");
 botao.addEventListener("click", function() {
     alert("Olá, estou aprendendo Javascript!");
 });
+
+const botaoMouse = document.querySelector("#botaoMouse");
+
+botaoMouse.addEventListener("mouseover", function() {
+    alert("O mouse passou por cima do botão!");
+});
+
